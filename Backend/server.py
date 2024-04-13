7ff31329e84fafbb88bf36d82bdf75397d0f1a05
Ross-Coombs <coombs.ross@gmail.com>
2024-04-13 16:11:06 +0100

    corrected rudder direction

--- Backend/server.py
+++ Backend/server.py
@@ -45,12 +45,12 @@ def adjustAngleForHat(actualAngle):
 def getCurrentHeading(currentHeading, rudderAngle):
 	#as this version is not attached to a real boat this function also simulates the boat turning, and as such requires rudderAngle and currentHeading
 	#if rudder is angled simulate turning the boat, turn boat faster the greater the rudder angle
-		if rudderAngle > 0:
-			currentHeading += int(rudderAngle/10)
+		if rudderAngle < 0:
+			currentHeading += -int(rudderAngle/10)
 			if currentHeading > 359:
 				currentHeading = currentHeading - 360
-		elif rudderAngle < 0:
-			currentHeading -= -int(rudderAngle/10)
+		elif rudderAngle > 0:
+			currentHeading -= int(rudderAngle/10)
 			if currentHeading < 0:
 				currentHeading = currentHeading + 360
 		return currentHeading
@@ -81,10 +81,10 @@ async def main():
 			rudderAngle = setRudderAngle(0)
 		#if quicker to turn right, turn right
 		elif shortestDist > 0:
-			rudderAngle = setRudderAngle(max(shortestDist, 10)) #rudder angle is greater, the bigger the turn, with a minimum of 10 degrees
+			rudderAngle = setRudderAngle(-max(shortestDist, 10)) #rudder angle is greater, the bigger the turn, with a minimum of 10 degrees
 		#if not, turn left
 		else:
-			rudderAngle = setRudderAngle(min(shortestDist, -10))
+			rudderAngle = setRudderAngle(-min(shortestDist, -10))
 		#Telemetry
 		# Check if there is an active websocket connection
 		if activeWebSocket:
